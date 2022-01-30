d093214073dc9a61bc3682c7f514834840673339
mohamad zbib <mz32programmer@gmail.com>
2022-01-30 20:36:10 +0200

    `Value` class example

---
+++ app/Config/Value.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Config;
+
+class Value
+{
+    /**
+     * The Value class is used for constants only
+     */
+    const ROLE = [
+        'GUEST' => 0,
+        'USER' => 1,
+        'ADMIN' => 2,
+    ];
+}
